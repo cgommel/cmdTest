1be06b95ae3e8e3c6da91cc11e5d1c700cc48b1a
Christoph Gommel <cgommel@me.com>
2013-09-11 22:47:16 +0200

    Initial commit

---
+++ cmdTest/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  cmdTest
+//
+//  Created by Christoph Gommel on 11.09.13.
+//  Copyright (c) 2013 Christoph Gommel. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
