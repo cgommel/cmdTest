1c7de6a5e784117a678407fdadfcc868e33756e7
Christoph Gommel <cgommel@me.com>
2013-09-11 23:57:30 +0200

    changed subtract order

--- cmdTest/main.c
+++ cmdTest/main.c
@@ -7,16 +7,16 @@
 //
 
 #include <stdio.h>
-#include "add.h"
 #include "subtract.h"
+#include "add.h"
 int main(int argc, const char * argv[])
 {
 
     // insert code here...
     printf("Hello, World!\n");
     printf("Hey, I am on github!\n");
-    printf("1+1 = %d\n",add(1,1));
     printf("2-1 = %d\n",subtract(2,1));
+    printf("1+1 = %d\n",add(1,1));
     return 0;
 }
 
